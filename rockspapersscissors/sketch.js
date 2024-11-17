10033a15b808924ff6f29232592bdcb283ed3d2f
rainner <rainner_hmm@outlook.com>
2024-11-17 17:22:26 -0600

    forgot the ref

--- rockspapersscissors/sketch.js
+++ rockspapersscissors/sketch.js
@@ -2,6 +2,13 @@
 // Rainn Morphy
 // September 25th, 2024 / Nov 17th, 2024
 
+// ref
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
+// https://github.com/Jordan514/rock_paper_scissors/blob/main/script.js
+// https://www.reddit.com/r/learnjavascript/comments/1cla2el/rock_paper_scissors_a_more_efficient_way/
+
+
 const playerChoices = ["rocks", "papers", "scissors"];
 const enemyChoices = structuredClone(playerChoices);
 
@@ -53,29 +60,7 @@ function draw() {
   else {
     titleScreen();
   }
-  switch (expr) {
-  case "Oranges":
-    console.log("Oranges are $0.59 a pound.");
-    break;
-  case "Apples":
-    console.log("Apples are $0.32 a pound.");
-    break;
-  case "Bananas":
-    console.log("Bananas are $0.48 a pound.");
-    break;
-  case "Cherries":
-    console.log("Cherries are $3.00 a pound.");
-    break;
-  case "Mangoes":
-  case "Papayas":
-    console.log("Mangoes and papayas are $2.79 a pound.");
-    break;
-  default:
-    console.log(`Sorry, we are out of ${expr}.`);
-}
-
-console.log("Is there anything else you'd like?");
-
+  
 }
 
 function mousePressed() {
